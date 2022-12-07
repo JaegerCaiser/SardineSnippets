cd260d25e48e31a8f8c14792ecf0d5ced30a95f9
Matheus Caiser <matheus.kaiser@gmail.com>
2022-12-07 13:27:27 -0300

    Sample typescript

--- sample.ts
+++ sample.ts
@@ -1,1 +1,22 @@
+interface Motorcycle {
+    handlebar: string
+}
 
+interface Car {
+    windshield: string
+}
+
+//Type Narrowing with type Predicate
+function isCar(vehicle: Motorcycle | Car): vehicle is Car {
+    return 'windshield' in vehicle;
+}
+
+//Promise function with, async, then, catch and await
+async function findBadGuys(filter: Record<string, string>): Promise<BadGuys> {
+    try{
+        await axios.get('/badguys/search', { data: filter })
+        .then((data) => { console.log(data) })
+    } catch (e) {
+        console.error(e);
+    }
+}
